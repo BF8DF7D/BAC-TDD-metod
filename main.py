91cad6eb6586f63337c253a137b5a4ae91ed877e
Атомный смеситель <nikitosik0202@yandex.ru>
2025-02-19 20:33:01 +0700

    Тест класса Поставщик:
    Создание экземпляра

--- main.py
+++ main.py
@@ -12,17 +12,30 @@ class TestVendor(unittest.TestCase):
         q, num = vend.getNumber();
         self.assertEqual(q, len(num));
         
-        
-
+# Тест класса Управляющий
+class TestManager(unittest.TestCase):
+    # Тест класса Управляющий: Создание экземпляра
+    def test_Manager(self):
+        manag = Manager();
+        self.assertIsNotNone(manag);
 
 
 # Поставщик
 class Vendor:
     # Метод Получения числа
     def getNumber(self):
-        # todo реализовать метод получения выходного значения
+        # todo реализовать метод получения return значения
         return [3, [1, 2, 3]]; # [Кол-во цифр в числе, [cамо число]]
         
+# Управляющий 
+# Cледит за ходом игры и устанавливает правила
+class Manager:
+    # todo методы сравнения результатов угадывания.
+    # todo методы выбора сложности игры.
+    # todo методы выбора режима игры.
+    None;
+
+
 if __name__ == '__main__':
     unittest.main();
     
