4289d93807a9964ef498c5deaf9d7abcc1e35b9f
Атомный смеситель <nikitosik0202@yandex.ru>
2025-02-19 20:16:52 +0700

    Тест метод "Получение числа":
    Корректность return значений

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import unittest
 
 # Тест класса Поставщик
 class TestVendor(unittest.TestCase):
+    # Тест класса Поставщик: Создание экземпляра
     def test_crateVendor(self):
         vend = Vendor();
         self.assertIsNotNone(vend);
+    # Тест метода Получение числа: Корректность return значений 
+    def test_return_getNumber(self):
+        vend = Vendor();
+        q, num = vend.getNumber();
+        self.assertEqual(q, len(num));
+        
+        
+
+
 
 # Поставщик
-# класс предоставляющий угадываемое число
 class Vendor:
-    def __init__(self):
-        # todo реализовать методы класса Поставщика
-        None;
+    # Метод Получения числа
+    def getNumber(self):
+        # todo реализовать метод получения выходного значения
+        return [3, [1, 2, 3]]; # [Кол-во цифр в числе, [cамо число]]
         
 if __name__ == '__main__':
     unittest.main();
